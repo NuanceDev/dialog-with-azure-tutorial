f56989975e02bae76a8f1f5ac6cb84795c58142a
Vincent Fournier <v.fournier11@gmail.com>
2019-02-18 14:46:20 -0500

    Add v0.0.7 and v1.0.0

--- coffee_CD-PRICE/index.js
+++ coffee_CD-PRICE/index.js
@@ -1,28 +1,10 @@
 module.exports = async function (context, req) {
 
-    var coffeeSize = req.body.task.CoffeeSize;
-    var coffeeType = req.body.task.CoffeeType;
-
-    // We calculate the price of the coffee
-    var coffeePrice = 2;
-
-    // Fancy coffees cost one dollar more
-    var fancyCoffeeTypes = ['cap', 'latte', 'mocha'];
-    if (fancyCoffeeTypes.includes(coffeeType)) {
-        coffeePrice += 1;
-    }
-
-    // Bigger coffee cost more
-    if (coffeeSize === 'md') {
-        coffeePrice += 0.4;
-    } else if (coffeeSize === 'lg') {
-        coffeePrice += 0.8;
-    }
-
+    // All coffee types and sizes at only 1$
     context.res = {
         body: {
             "data": [{
-                "value": coffeePrice
+                "value": 1
             }]
         }
     };
